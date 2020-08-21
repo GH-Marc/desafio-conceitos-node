7300c3040e6b04e93cdf14f0c00b3384f1b21d6e
Marcelo Silva <marcelopgm65@gmail.com>
2020-08-21 18:55:10 -0300

    challenge done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const project = { 
+    id: uuid(), 
+    title , 
+    url , 
+    techs , 
+    likes: 0 
+  }
+
+  repositories.push(project);
+
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const projectIndex = repositories.findIndex(project => project.id == id);
+
+  if (projectIndex == -1) {
+    return response.status(400).json({ error: 'Repository does not exists.'});
+  }
+    const project = {
+      id,
+      title,
+      url,
+      techs,
+      likes: repositories[projectIndex].likes,
+    };
+
+    repositories[projectIndex] = project;
+
+    return response.json(project);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = repositories.findIndex(project => project.id == id);
+
+  if (projectIndex >= 0) {
+    repositories.splice(projectIndex, 1);
+  } else {
+    return response.status(400).json({ error: 'Invalid repository'});
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = repositories.findIndex(project => project.id == id);
+
+  if (projectIndex == -1) {
+    return response.status(400).json({ error: 'Repository does not exists.'});
+  }
+
+  repositories[projectIndex].likes++;
+
+  return response.json(repositories[projectIndex]);
 });
 
 module.exports = app;
